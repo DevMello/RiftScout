2016c917df7e2605c8bd9fc34c9af58b1a531eef
DevMello <kol.kid.py@gmail.com>
2024-11-26 12:43:21 -0800

    typo fix

--- app/page.tsx
+++ app/page.tsx
@@ -386,7 +386,7 @@ const calculateWinningProbability = (match: any) => {
           </div>
           <div className="relative flex justify-center text-xs uppercase">
             <span className="bg-background px-2 text-muted-foreground">
-              Or
+              And
             </span>
           </div>
         </div>
